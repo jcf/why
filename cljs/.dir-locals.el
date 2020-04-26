d45f411361f8cbfc90b12dffb74b01725f1691ce
James Conroy-Finn <git@invetica.co.uk>
2020-04-26 21:14:59 +0100

    Instruct Emacs in the ways of Cider
    Would love a way to avoid polluting repos with configuration for
    individual editors. I wonder if Clojure tooling could agree on
    namespaced keywords in a `deps.edn` somewhere…
    
        {:cider/known-endpoints      (("localhost" "8777"))
         :cider/preferred-build-tool clojure-cli}

---
+++ cljs/.dir-locals.el
@@ -0,0 +1,5 @@
+((nil
+   (cider-clojure-cli-global-options . "-A:dev:test")
+   (cider-default-cljs-repl . shadow-select)
+   (cider-known-endpoints . (("localhost" "8777")))
+   (cider-preferred-build-tool . clojure-cli)))
